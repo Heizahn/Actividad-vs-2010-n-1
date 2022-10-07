fbc8721408377709c1caf01f9407d55fa25aa4f1
Heizahn <hrbweibezahn@gmail.com>
2022-10-07 15:49:07 -0400

    actividad 1 realizada

---
+++ Actividad1/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Dim diametro, radio As Decimal
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        diametro = TextBox1.Text
+        radio = diametro / 2
+
+        Label2.Text = ("El radio es: " & radio)
+    End Sub
+End Class
